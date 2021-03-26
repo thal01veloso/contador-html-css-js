5920d9f42c9e589cd859ad85315415d0322ec6ef
thal01veloso <thal01veloso@gmail.com>
2021-03-26 08:52:03 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var a=0;
+function conta() {
+    a+=1;
+    return document.getElementById("tela").value = a;
+
+}
+function diminui(){
+    a-=1;
+    return document.getElementById("tela").value = a;
+
+}   
